62e8df28f468663063db6e0be36bd04b5526afed
OKtol <oktol.hz@gmail.com>
2023-01-11 16:03:15 +0300

    simplified using

--- e-sandbox/Power/Program.cs
+++ e-sandbox/Power/Program.cs
@@ -4,10 +4,8 @@
     {
         static void Main(string[] args)
         {
-            using (var game = new Game(800, 600, "kek"))
-            {
-                game.Run();
-            }
+            using var game = new Game(800, 600, "kek");
+            game.Run();
         }
     }
 }
