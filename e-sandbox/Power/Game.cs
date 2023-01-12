853d82628274c5ad7872e37f2912c8fe07a9df4c
OKtol <oktol.hz@gmail.com>
2023-01-12 20:24:28 +0300

    triangle and shaders....

--- e-sandbox/Power/Game.cs
+++ e-sandbox/Power/Game.cs
@@ -1,4 +1,7 @@
-﻿using OpenTK.Windowing.Desktop;
+﻿using OpenTK.Graphics.OpenGL4;
+using OpenTK.Mathematics;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,11 +12,49 @@ namespace Power
 {
     internal class Game : GameWindow
     {
+        private int _vertexBufferObject;
+
         public Game(int width, int height, string title) : base(
             GameWindowSettings.Default,
             new NativeWindowSettings() { Size = (width, height), Title = title })
         {
 
+        }
+
+        protected override void OnLoad()
+        {
+            base.OnLoad();
+
+            GL.ClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+
+            _vertexBufferObject = GL.GenBuffer();
+            GL.BindBuffer(BufferTarget.ArrayBuffer, _vertexBufferObject);
+
+            float[] vertices = {
+                -0.5f, -0.5f, 0.0f, //Bottom-left vertex
+                 0.5f, -0.5f, 0.0f, //Bottom-right vertex
+                 0.0f,  0.5f, 0.0f  //Top vertex
+            };
+
+            GL.BufferData(BufferTarget.ArrayBuffer,
+                vertices.Length * sizeof(float), vertices,
+                BufferUsageHint.StaticDraw);
+        }
+
+        protected override void OnRenderFrame(FrameEventArgs args)
+        {
+            base.OnRenderFrame(args);
+
+            GL.Clear(ClearBufferMask.ColorBufferBit);
+
+            SwapBuffers();
+        }
+
+        protected override void OnResize(ResizeEventArgs e)
+        {
+            base.OnResize(e);
+
+            GL.Viewport(0, 0, e.Width, e.Height);
         }
     }
 }
